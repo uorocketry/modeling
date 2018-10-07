f9b07acfd810e7361e6a9fcb87aa4109556ecb8a
mginoya <ginoya.manit@gmail.com>
2018-10-07 17:51:27 -0400

    Added shells for basis classes, Element and Simulation

---
+++ basis/Element.m
@@ -0,0 +1,18 @@
+classdef Element < handle
+    %ELEMENT Abstract parent class that all elements will inherit from;
+    %   These elements will be used to model anything and everything that
+    %   uorocketry needs to perform studies upon
+    %==================================================================
+    % Date Created: Oct 7th 2018
+    %==================================================================
+    
+    properties
+        
+    end
+    
+    methods
+        function obj = Element()
+        end
+    end
+end
+
---
+++ basis/Simulation.m
@@ -0,0 +1,17 @@
+classdef Simulation < handle
+    %SIMULATION Class that is responsible for configuring, running, and
+    %   post-processing a simulation
+    %==================================================================
+    % Date Created: Oct 7th 2018
+    %==================================================================
+    
+    properties
+        
+    end
+    
+    methods
+        function obj = Simulation()
+        end
+    end
+end
+
