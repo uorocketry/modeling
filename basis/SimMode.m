bea4047a0ad6a20fa6339b8f9dca4cda5732254c
mginoya <ginoya.manit@gmail.com>
2018-10-08 19:33:19 -0400

    Added enumeration classes that will be useful to capture the state and mode of the simulation

---
+++ basis/SimMode.m
@@ -0,0 +1,16 @@
+classdef SimMode
+    %SIMMODE Enumerated list of allowable simulation modes
+    
+    enumeration
+        NORM
+        ACCEL
+        RAPIDACCEL
+    end
+    
+    properties
+    end
+    
+    methods
+    end
+end
+
---
+++ basis/SimState.m
@@ -0,0 +1,19 @@
+classdef SimState
+    %SIMSTATE Enumerated list of allowable simulation states
+    
+    enumeration
+        INITIALIZED
+        CONFIGURED
+        COMPLETED
+        LOGS_GENERATED
+        POST_PROCESSED
+        ERROR
+    end
+    
+    properties
+    end
+    
+    methods
+    end
+end
+
