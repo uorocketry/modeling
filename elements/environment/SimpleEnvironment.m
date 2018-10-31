483029ef147e83bf8fb8392377813a250181f62f
mginoya <ginoya.manit@gmail.com>
2018-10-31 08:17:26 -0400

    Added shell files for elements and master library

---
+++ elements/environment/SimpleEnvironment.m
@@ -0,0 +1,13 @@
+classdef SimpleEnvironment < Element
+    %SIMPLEENVIRONMENT Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+    end
+    
+    methods
+        function obj = SimpleEnvironment()
+        end
+    end
+end
+
---
+++ elements/propulsion/SolidMotor.m
@@ -0,0 +1,13 @@
+classdef SolidMotor < Element
+    %SOLIDMOTOR Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+    end
+    
+    methods
+        function obj = SolidMotor()
+        end
+    end
+end
+
