071aba9e4f0ca91f50ddb109091a06751566fe99
mginoya <ginoya.manit@gmail.com>
2019-01-21 15:38:49 -0500

    Added more parameters for aero class

--- elements/aero/Aero.m
+++ elements/aero/Aero.m
@@ -7,22 +7,46 @@ classdef Aero < Element
         InertiaMatNoEngine
         
         %body diameters
-        dn
-        db
-        du
+        dn                              % diameter at nosecone base
+        db                              % average diameter of body
+        df                              % diameter of body at leading point of fin
+        du                              % diameter at body at starting of tail cone
+        dd                              % diameter of bottom on tail cone
         
         %body lengths
-        ln
-        lb
-        lc
-        l_TS
-        l_TR
+        ln                              % length of nosecone
+        lb                              % length of body
+        lc                              % length of tailcone
+        l_TS                            % total horizontal length (ie. fin tip to fin tip)
+        l_TR                            % total vertical length of rocket
+        
+        %body areas
+        Ar                              % reference area (base of nosecone)
+        A_fp                            % fin planform area
+        A_fe                            % exposed fin planform area
         
         %fin geometry
-        lr
-        lt
-        lm
-        lf
+        lr                              % fin root length
+        lt                              % fin tip length
+        lw                              % fin root length - fin tip length
+        lm                              % fin major length
+        ls                              % fin horizontal length
+        tf                              % fin thickness
+        nf                              % number of fins
+        
+        % Inportant Locations
+        Xf                              %Fin location from nosetip
+        Xc                              %Tail cone location from nosetip
+        Xcp                             %center of pressure location from nosetip
+        
+        % other coeffs
+        K_BF                            % coefficient that accounts for increase in normal force due to fin interference
+        Rs                              % fin section ratio
+        k_fb                            % fin-body infereference coeff 
+        k_bf                            % body-fin intereference coeff
+        del                             % experimental coeff 1 (to calculate AoA effects)
+        eta                             % experimental coeff 2 (to calculate AoA effects)
+        
     end
     
     methods
