d9ee2547fb82a958ad0cd0ec4a09a24c9da7d986
mginoya <ginoya.manit@gmail.com>
2019-04-19 17:25:05 -0400

    Tested and fixed FSv1, it is now ready for use

--- studies/fsTesting.m
+++ studies/fsTesting.m
@@ -3,22 +3,32 @@ elem.initialize();
 
 sim = Simulation(elem);
 
-sim.endTime = 100;
-sim.timeProfile = 0:sim.simElement.timeStep:sim.endTime;
-
+sim.endTime = 10;
+sim.timeProfile = [0 2 4 6 8 9 sim.endTime];
+ 
 sim.simInput = struct();
-sim.simInput.position_E = [1 2 3;
-                           1 2 3;
-                           1 2 3];
-sim.simInput.orientation = [1 2 3;
-                            1 2 3;
-                            1 2 3];
-sim.simInput.velocity_E = [1 2 3;
-                           1 2 3;
-                           1 2 3];
-sim.simInput.thrust_B = [1 2 3;
-                         1 0 0;
-                         1 0 0];
+sim.simInput.position_E = [0 0   0    0   0   0 0;
+                           0 0   0    0   0   0 0;
+                           0 500 3000 200 100 10 0];
+                       
+sim.simInput.orientation = [0 0 0 0 0 0 0;
+                            0 0 0 0 0 0 0;
+                            0 0 0 0 0 0 0];
+                        
+sim.simInput.velocity_E = [0 0   0 0   0  0 0; 
+                           0 0   0 0   0  0 0;
+                           0 300 0 -20 -9 0 0];
+                       
+sim.simInput.descentOneDeploy = [0 0 0 0 1 1 1];
 
-bdclose all;
-sim.configureModel();
+sim.simInput.descentTwoDeploy = [0 0 0 0 0 1 1];
+
+sim.simInput.thrust_B = [1000 1000 0 0 0 0 0;
+                         0    0    0 0 0 0 0;
+                         0    0    0 0 0 0 0];
+
+%bdclose all;
+%sim.configureModel();
+%sim.openModel();
+
+sim.run();
