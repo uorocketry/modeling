fed2d1b6afeb6f13be94403e03a0d92c06b560e6
mginoya <ginoya.manit@gmail.com>
2019-10-28 00:35:07 -0400

    finished the first iteration of Hybrid Rocket Model, courtesy of Calvin, May, Liam and Ashi :)

--- studies/hreTesting.m
+++ studies/hreTesting.m
@@ -3,14 +3,19 @@ elem.initialize();
 
 sim = Simulation(elem);
 
-sim.endTime = 5.1;
-
+sim.endTime = 20;
 sim.timeProfile = 0:sim.simElement.timeStep:sim.endTime;
 
+% setting up inputs (do not change as these are the control conditions)
 sim.simInput = struct();
 sim.simInput.time = sim.timeProfile;
+sim.simInput.valveCmd = 0*sim.timeProfile;
+sim.simInput.temp_atm = 25*ones(length(sim.timeProfile));
+sim.simInput.pressure_atm = 101.5*ones(length(sim.timeProfile));
+sim.simInput.density_atm = 1.225*ones(length(sim.timeProfile));
 
-%bdclose all;
-%sim.configureModel();
+bdclose all;
+sim.configureModel();
+sim.openModel();
 
-sim.run();
+%sim.run();
