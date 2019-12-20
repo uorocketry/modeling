aa96a52cf437230e47edfa73f0a467f687513e3f
betato <battlestargatticus@gmail.com>
2019-12-20 17:28:31 -0500

    add normally distributed 'random' wind speed selection

--- elements/environment/SimpleEnv.m
+++ elements/environment/SimpleEnv.m
@@ -8,9 +8,18 @@ classdef SimpleEnv < Element
         atmospheric_coeff                           % emperial coefficient for wind speed calculation
         
         % measured wind parameters
-        referenceWindSpeed                          % the 'measured' wind speed as a reference                [m/s]
-        referenceWindAltitude                       % the altitude where reference speed was 'measured'       [m]
-        referenceWindAngle                          % the angle of wind (ie. encoded wind direction)          [rad]
+        referenceWindAltitude                       % the altitude where reference speed was 'measured'                                     [m]
+        referenceWindSpeed                          % the 'measured' wind speed as a reference                                              [m/s] 
+        referenceWindAngle                          % the angle of wind (ie. encoded wind direction)                                        [rad]
+        
+        % parameters for wind variation
+        referenceWindSpeedVarience                  % normal distribution varience from the reference wind speed for random wind generation     []
+        referenceWindAngleVarience                  % normal distribution varience from the reference wind angle for random wind directions     []
+        randomSeed                                  % numerical seed required to generate a uniue random value. change for each simulation run  []
+        
+        % launch location
+        launchLatitude                              % latitude of the launch site                             [rad]
+        launchLongditude                            % longditude of the launch site                           [rad]
         
         % measured static conditions used to interpolate
         referenceStaticAltitude                     % the altitude where static conditions were 'measured'    [m]
@@ -21,12 +30,16 @@ classdef SimpleEnv < Element
         
         % emperical dynamic viscosity of air
         altiBreakpoints                             % altitude (ASL) breakpoints                              [m]
+        temperatureAir                              % air temperature                                         [C]
+        pressureAir                                 % air pressure                                            [Pa]
+        densityAiR                                  % air density                                             [kg/m^3]
+        speedOfSound                                % speed of sound through atmosphere                       [m/s]
         dynamicViscosityAir                         % dynamic viscosity table data                            [N s/m^2]
         
         % Earth params
         RADIUS_EARTH = 6371*10^3                    % radius of Earth                                         [m]
-        MAJOR_RADIUS_EARTH = 6378137.0              % WGS-84 ellipsoid, semi-minor axis
-        MINOR_RADIUS_EARTH = 6356752.3              % WGS-84 ellipsoid, semi-major axis
+        MAJOR_RADIUS_EARTH = 6378137.0              % WGS-84 ellipsoid, semi-minor axis                       [m]
+        MINOR_RADIUS_EARTH = 6356752.3              % WGS-84 ellipsoid, semi-major axis                       [m]
         MASS_EARTH = 5.972*10^24                    % mass of Earth                                           [kg]
         ANGULARVELOCITY_EARTH = 7.2921159*10^(-5)   % angular velocity of Earth                               [rad/s]
         
