a5c02932890d70b8c6c7d88314036a8a54d6bfa6
Ashi Dissanayake <adiss010@uottawa.ca>
2020-08-13 18:49:48 +0530

    Hot fire

---
+++ studies/HREWithHotFireTesting.m
@@ -0,0 +1,36 @@
+elem = HybridEngine('HREHotFireData');
+elem.initialize();
+
+% Primary Design Parameters for Hybrid Engine
+elem.oxiTank.initOxiTemp = 20;          % Temperature of nitrous after fill        [deg c]
+elem.A_ih = 2.4476e-6;                  % area of single injector hole  [m^2]
+elem.nIHoles = 16;                      % number of holes in injector   [dimless]
+elem.At = 1e-3;                         % area of nozzle throat         [m^2]
+elem.Ae = 0.0058;                       % area of nozzle outlet         [m^2]
+elem.Dfg_inner = 0.04;                  % initial inner diameter of fuel grain      [m]
+elem.Dfg_outer = 0.134;                 % outer diameter of fuel grain              [m]
+elem.Lfg = 0.3;                         % length of fuel grain                      [m]
+
+sim = Simulation(elem);
+
+sim.endTime = 12;
+sim.timeProfile = 0:sim.simElement.timeStep:sim.endTime;
+
+% setting up inputs (do not change as these are the control conditions)
+sim.simInput = struct();
+sim.simInput.time = sim.timeProfile;
+sim.simInput.valveCmd = 0*sim.timeProfile;
+sim.simInput.temp_atm = 25.*ones(size(sim.timeProfile));
+sim.simInput.pressure_atm = 101.3.*ones(size(sim.timeProfile));
+sim.simInput.density_atm = 1.225.*ones(size(sim.timeProfile));
+sim.simInput.mass_oxi = 1.225.*ones(size(sim.timeProfile));
+sim.simInput.density_oxi = 1.225.*ones(size(sim.timeProfile));
+sim.simInput.Tank_pressure = 1.225.*ones(size(sim.timeProfile));
+sim.simInput.Pchamber = 1.225.*ones(size(sim.timeProfile));
+
+
+bdclose all;
+sim.configureModel();
+sim.openModel();
+
+%sim.run();
